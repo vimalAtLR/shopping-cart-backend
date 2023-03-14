54546d767247d70a8690fbf76c8a92ace8147181
vimalAtLR <vimal@logicrays.com>
2023-03-14 19:11:14 +0530

    changed image url

--- products.js
+++ products.js
@@ -6,7 +6,7 @@ const products = [
     desc: "6.1-inch display",
     price: 999,
     image:
-      "https://res.cloudinary.com/chaoocharles/image/upload/v1629289889/online-shop/iphone12pro_e09phn.jpg",
+      "https://i.gadgets360cdn.com/large/iphone_12_pro_max_review_cover_1606199523181.jpg?downsize=950:*",
   },
   {
     id: 2,
@@ -15,7 +15,7 @@ const products = [
     desc: "5.4-inch mini display",
     price: 699,
     image:
-      "https://res.cloudinary.com/chaoocharles/image/upload/v1629289889/online-shop/iphone12_efhrcp.jpg",
+      "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-13-mini-blue-witb-2021_FMT_WHH?wid=560&hei=744&fmt=jpeg&qlt=90&.v=1638579083000",
   },
   {
     id: 3,
@@ -24,7 +24,7 @@ const products = [
     desc: "6.5-inch display",
     price: 399,
     image:
-      "https://res.cloudinary.com/chaoocharles/image/upload/v1629289889/online-shop/galaxyS_dvjf5w.png",
+      "https://www.apple.com/newsroom/images/product/iphone/standard/Apple-iPhone-14-iPhone-14-Plus-hero-220907_Full-Bleed-Image.jpg.large.jpg",
   },
 ];
 
